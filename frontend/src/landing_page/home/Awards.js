148ebf63dc6d36b280a7761edd7512c186a94d35
Shivam Riyar <shivamriyar123@gmail.com>
2025-03-19 23:59:45 +0530

    Update Awards.js

--- frontend/src/landing_page/home/Awards.js
+++ frontend/src/landing_page/home/Awards.js
@@ -7,6 +7,39 @@ function Awards() {
                 <div className='col-6'>
                     <img src='media/images/largestBroker.svg'></img>
                 </div>
+                <div className='col-6'>
+                    <h1>Largest stock broker in India</h1>
+                    <p>2+ million Zerodha clients contribute to over 15% of all retail order volumes in India daily by trading and investing in:</p>
+                    <div className='row'>
+                        <div className='col-6'>
+                        <ul>
+                        <li>
+                            <p>Futures and Options</p>
+                        </li>
+                        <li>
+                            <p>Commodity derivatives</p>
+                        </li>
+                        <li>
+                            <p>Currency derivatives</p>
+                        </li>
+                        </ul>
+                        </div>
+                        <div className='col-6'>
+                            <ul>
+                            <li>
+                            <p>Stocks & IPOs</p>
+                        </li>
+                        <li>
+                            <p>Direct mutual funds</p>
+                        </li>
+                        <li>
+                            <p>Bonds and Govt. Securitries</p>
+                        </li>
+                            </ul>
+                        </div>
+                    </div>
+                    
+                </div>
             </div>
         </div>
      );
