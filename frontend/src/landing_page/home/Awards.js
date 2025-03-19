af3978ee367b6839ed6dbda027e2a37d3eb5a0e5
Shivam Riyar <shivamriyar123@gmail.com>
2025-03-20 03:08:13 +0530

    Stats.js

--- frontend/src/landing_page/home/Awards.js
+++ frontend/src/landing_page/home/Awards.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 function Awards() {
     return ( 
-        <div className='container'>
+        <div className='container mt-5'>
             <div className='row'>
-                <div className='col-6'>
+                <div className='col-6 p-5'>
                     <img src='media/images/largestBroker.svg'></img>
                 </div>
-                <div className='col-6'>
+                <div className='col-6 p-5 mt-3'>
                     <h1>Largest stock broker in India</h1>
-                    <p>2+ million Zerodha clients contribute to over 15% of all retail order volumes in India daily by trading and investing in:</p>
+                    <p className='mb-5'>2+ million Zerodha clients contribute to over 15% of all retail order volumes in India daily by trading and investing in:</p>
                     <div className='row'>
                         <div className='col-6'>
                         <ul>
@@ -38,7 +38,7 @@ function Awards() {
                             </ul>
                         </div>
                     </div>
-                    <img src='media/images/pressLogos.png'></img>
+                    <img src='media/images/pressLogos.png' style={{width:"90%"}}></img>
                 </div>
             </div>
         </div>
