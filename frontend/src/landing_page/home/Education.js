fd0efacd7fa7b64fcedcbe00b078f120020f214d
Shivam Riyar <shivamriyar123@gmail.com>
2025-03-21 23:50:16 +0530

    Update Education.js

--- frontend/src/landing_page/home/Education.js
+++ frontend/src/landing_page/home/Education.js
@@ -1,7 +1,17 @@
 import React from 'react';
 function Education() {
     return (
-        <h1>Education</h1>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-6'>
+                <h1 className='mb-3'>Free and open market education</h1>
+                <p>Varsity, the largest online stock market education book in the world covering everything from the basics to advanced trading</p>
+            </div>
+            <div className='col-6'>
+
+            </div>
+          </div>
+        </div>
       );
 }
 
