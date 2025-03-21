3794200b327bd634a587e02e6fa53e27f6a879cf
Shivam Riyar <shivamriyar123@gmail.com>
2025-03-21 23:56:10 +0530

    Update Education.js

--- frontend/src/landing_page/home/Education.js
+++ frontend/src/landing_page/home/Education.js
@@ -4,10 +4,10 @@ function Education() {
         <div className='container'>
           <div className='row'>
             <div className='col-6'>
-                <img src='media/images/education.svg'></img>
+                <img src='media/images/education.svg' style={{width:"70%"}}></img>
             </div>
             <div className='col-6'>
-            <h1 className='mb-3'>Free and open market education</h1>
+            <h1 className='mb-3 fs-2'>Free and open market education</h1>
             <p>Varsity, the largest online stock market education book in the world covering everything from the basics to advanced trading</p>
             <a href='' style={{textDecoration:"none"}}>Versity</a>
             <p className='mt-5'>TradingQ&A, the most active trading and invesment community in India for all your</p>
