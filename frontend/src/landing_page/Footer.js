150bf581d9d5656fc9d3988f168da7916bfbb596
Shivam Riyar <shivamriyar123@gmail.com>
2025-03-29 23:26:15 +0530

    Update Footer.js

--- frontend/src/landing_page/Footer.js
+++ frontend/src/landing_page/Footer.js
@@ -10,12 +10,33 @@ function Footer() {
                 </div>
                 <div className='col'>
                     <p>Company</p>
+                    <a href=''>About</a><br/>
+                    <a href=''>Pricing</a><br/>
+                    <a href=''>Referral programme</a><br/>
+                    <a href=''>Products</a><br/>
+                    <a href=''>Careers</a><br/>
+                    <a href=''>Zerodha.tech</a><br/>
+                    <a href=''>Open source</a><br/>
+                    <a href=''>Press & media</a><br/>
+                    <a href=''>Zerodha Cares (CSR)</a><br/>
                 </div>
                 <div className='col'>
                     <p>Support</p>
+                    <a href=''>Contact us</a><br/>
+                    <a href=''>Support portal</a><br/>
+                    <a href=''>Z-Connect blog</a><br/>
+                    <a href=''>List of charges</a><br/>
+                    <a href=''>Downloads & resources</a><br/>
+                    <a href=''>Videos</a><br/>
+                    <a href=''>Market overview</a><br/>
+                    <a href=''>How to file a complaint?</a><br/>
+                    <a href=''>Status of your complaints</a><br/>
+
                 </div>
                 <div className='col'>
                     <p>Account</p>
+                    <a href=''>Open an account</a><br/>
+                    <a href=''>Fund transfer</a><br/>               
                 </div>
 
             </div>
