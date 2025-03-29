146d6cca0aa15cb745267da80b052f72fdbbb4a2
Shivam Riyar <shivamriyar123@gmail.com>
2025-03-29 22:55:33 +0530

    footer and Hero.js

--- frontend/src/landing_page/Footer.js
+++ frontend/src/landing_page/Footer.js
@@ -1,9 +1,27 @@
 import React from 'react';
 
 function Footer() {
-    return ( <h1>
-        footer
-    </h1> );
+    return ( 
+        <div className='container'>
+            <div className='row'>
+                <div className='col'>
+                    <img src='media/images/logo.svg' style={{width:"50%"}}></img>
+
+                </div>
+                <div className='col'>
+                    <p>Company</p>
+                </div>
+                <div className='col'>
+                    <p>Support</p>
+                </div>
+                <div className='col'>
+                    <p>Account</p>
+                </div>
+
+            </div>
+        </div>
+
+    );
 }
 
 export default Footer;
--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -1,16 +1,17 @@
 import React from 'react';
 function Hero() {
     return ( 
-        <div className='container p-5'>
+        <div className='container p-5' style={{align:"center"}}>
             <div className='row text-center'>
-                <img src='media/images/homeHero.png' alt='Hero Image' className='mb-5'/>
-                <h1 className='mt-5'>
+                <img src='media/images/homeHero.png' alt='Hero Image' style={{width:"60%",display:"block",
+  margin:"auto"}} className='mb-5'/>
+                <h1 className='mt-5 fs-1'>
                     Invest in Everything.
                 </h1>
-                <p>
+                <p className='fs-5'>
                     Online Platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.
                 </p>
-                <button className='p-3 btn btn-primary fs-5' style={{width:"20%", margin:"0 auto"}}>Signup Now</button>
+                <button className='p-1 btn btn-primary fs-5' style={{width:"20%", margin:"0 auto"}}>Sign up for free</button>
             </div>
         </div>
         
